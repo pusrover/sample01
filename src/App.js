dc62e1696029d4204323167a9aeba82dd6e2e6f8
pusrover <pusrover@gmail.com>
2025-06-17 14:10:03 +0900

    ch19 까지 예제 작성

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
 
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: 'read',
       selected_content_id : 2,
@@ -45,7 +46,17 @@ class App extends Component {
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if(this.state.mode === 'create'){
-      _article = <CreateContent></CreateContent>;
+      _article = <CreateContent
+        onSubmit={function(_title, _desc){
+          this.max_content_id = this.max_content_id + 1;
+          var _content = this.state.contents.concat(
+            {id : this.max_content_id, title : _title, desc : _desc}
+          );
+          this.setState({
+            contents : _content});
+          console.log(_title, _desc);
+          }.bind(this)}>
+      </CreateContent>;
     }
     return(
       <div className='App'>
